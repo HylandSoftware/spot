c9cf80436d3b9222ad259319de796a38a018b879
Nathan Lowe <techwiz96@gmail.com>
2018-07-07 12:42:02 -0400

    Build docker image on travis

--- build/travis.sh
+++ build/travis.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-make restore
-make test
-
+make
 "${GOPATH}/bin/goveralls" -coverprofile="${SCRIPT_ROOT}/../coverage.out" -service=travis-ci
+
+CGO_ENABLED=0 make build-unix
+docker build "${SCRIPT_ROOT}/../" -f "${SCRIPT_ROOT}/Dockerfile" -t hylandsoftware/spot
