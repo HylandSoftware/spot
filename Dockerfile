177d5d1e03f27501075e674d0aea16fd1fdef55e
Nathan Lowe <nathan.lowe@hyland.com>
2018-08-09 12:25:16 -0400

    Publish to Docker Hub on master

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,4 @@
-FROM golang:1.10 AS builder
-
-ADD . /go/src/github.com/hylandsoftware/spot
-WORKDIR /go/src/github.com/hylandsoftware/spot
-RUN go get -u github.com/golang/dep/...
-RUN make restore && \
-    make test && \
-    CGO_ENABLED=0 GOOS=linux make build-unix
-
 FROM alpine
-COPY --from=builder /go/src/github.com/hylandsoftware/spot/dist/spot /spot
 
+ADD ./dist/spot /spot
 ENTRYPOINT ["/spot"]
--- build/travis.sh
+++ build/travis.sh
@@ -7,4 +7,18 @@ make
 "${GOPATH}/bin/goveralls" -coverprofile="${SCRIPT_ROOT}/../coverage.out" -service=travis-ci
 
 CGO_ENABLED=0 make build-unix
-docker build "${SCRIPT_ROOT}/../" -f "${SCRIPT_ROOT}/Dockerfile" -t hylandsoftware/spot
+
+IMAGE_TAG=ci
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    IMAGE_TAG=$(docker run -it --rm -v "$(pwd):/repo" gittools/gitversion /showvariable NuGetVersionV2 | tee /dev/tty)
+fi
+
+docker build "${SCRIPT_ROOT}/../" -f "${SCRIPT_ROOT}/../Dockerfile" -t "hylandsoftware/spot:${IMAGE_TAG%$'\r'}"
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+
+    docker tag "hylandsoftware/spot:${IMAGE_TAG%$'\r'}" "hylandsoftware/spot:latest"
+    docker push "hylandsoftware/spot:${IMAGE_TAG%$'\r'}"
+    docker push "hylandsoftware/spot:latest"
+fi
